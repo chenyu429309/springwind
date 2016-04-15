720e438946680072603068b541b3e92ef0e92c1a
fanzongyuan <63766031@qq.com>
2016-04-15 17:09:30 +0800

    加入用户管理列表页面，以及bootstrap-table的JS和CSS文件---Jack

---
+++ SpringWind/src/main/java/com/baomidou/springwind/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.baomidou.springwind.controller;
+
+import com.baomidou.mybatisplus.plugins.Page;
+import com.baomidou.springwind.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * <p>
+ * 用户管理相关操作
+ * </p>
+ *
+ *
+ * @Author Jack
+ * @Date 2016/4/15 15:03
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController extends BaseController {
+
+    @Autowired
+    private IUserService userService;
+
+    @RequestMapping("/toUserPage")
+    public String toUserPage(Model model){
+        return "/user/user";
+    }
+
+    @RequestMapping("/getUserList")
+    public Page getUserList(HttpServletRequest request, HttpServletResponse response){
+
+        return null;
+    }
+
+
+}
