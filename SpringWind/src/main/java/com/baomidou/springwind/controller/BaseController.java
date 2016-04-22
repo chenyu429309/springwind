f914649368f783788031be12b7637b9535215004
hubin <jobob@qq.com>
2016-04-22 16:33:14 +0800

    改为无地址刷新

--- SpringWind/src/main/java/com/baomidou/springwind/controller/BaseController.java
+++ SpringWind/src/main/java/com/baomidou/springwind/controller/BaseController.java
@@ -1,11 +1,17 @@
 package com.baomidou.springwind.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.alibaba.fastjson.JSONObject;
 import com.baomidou.framework.controller.SuperController;
 import com.baomidou.framework.mail.MailHelper;
 import com.baomidou.mybatisplus.plugins.Page;
+import com.baomidou.springwind.service.IPermissionService;
 import com.baomidou.springwind.service.IUserService;
 
 /**
@@ -16,14 +22,65 @@ import com.baomidou.springwind.service.IUserService;
  * @author hubin
  * @Date 2016-04-13
  */
-public class BaseController extends SuperController {
+public class BaseController extends SuperController implements HandlerInterceptor {
+
+	@Autowired
+	protected MailHelper mailHelper;
 
 	@Autowired
 	protected IUserService userService;
-	
+
 	@Autowired
-	protected MailHelper mailHelper;
-	
+	private IPermissionService permissionService;
+
+
+	@Override
+	public boolean preHandle( HttpServletRequest request, HttpServletResponse response, Object handler )
+		throws Exception {
+		return true;
+	}
+
+
+	@Override
+	public void postHandle( HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView ) throws Exception {
+		/*
+		 * 方法调用后调用该方法，返回数据给请求页
+		 */
+		if ( isLegalView(modelAndView) ) {
+			modelAndView.addObject("currentUser", userService.selectById(getCurrentUserId()));
+			modelAndView.addObject("menuList", permissionService.selectMenuVOByUserId(getCurrentUserId()));
+		}
+	}
+
+
+	@Override
+	public void afterCompletion( HttpServletRequest request, HttpServletResponse response, Object handler,
+			Exception ex ) throws Exception {
+	}
+
+
+	/**
+	 * 判断是否为合法的视图地址
+	 * <p>
+	 * 
+	 * @param modelAndView
+	 *            spring 视图对象
+	 * @return boolean
+	 */
+	protected boolean isLegalView( ModelAndView modelAndView ) {
+		boolean legal = false;
+		if ( modelAndView != null ) {
+			String viewUrl = modelAndView.getViewName();
+			if ( viewUrl != null && viewUrl.contains("redirect:") ) {
+				legal = false;
+			} else {
+				legal = true;
+			}
+		}
+		return legal;
+	}
+
 
 	/**
 	 * <p>
@@ -34,20 +91,21 @@ public class BaseController extends SuperController {
 	 *            分页对象
 	 * @return
 	 */
-	protected String jsonPage(Page<?> page) {
+	protected String jsonPage( Page<?> page ) {
 		JSONObject jo = new JSONObject();
 		jo.put("total", page.getTotal());
 		jo.put("rows", page.getRecords());
 		return toJson(jo);
 	}
-	
+
+
 	@Override
-	protected <T> Page<T> getPage(int size) {
+	protected <T> Page<T> getPage( int size ) {
 		int _size = size, _index = 1;
-		if (request.getParameter("_size") != null) {
+		if ( request.getParameter("_size") != null ) {
 			_size = Integer.parseInt(request.getParameter("_size"));
 		}
-		if (request.getParameter("_index") != null) {
+		if ( request.getParameter("_index") != null ) {
 			int _offset = Integer.parseInt(request.getParameter("_index"));
 			_index = _offset / _size + 1;
 		}
--- SpringWind/src/main/java/com/baomidou/springwind/controller/IndexController.java
+++ SpringWind/src/main/java/com/baomidou/springwind/controller/IndexController.java
@@ -1,6 +1,5 @@
 package com.baomidou.springwind.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.baomidou.kisso.annotation.Action;
 import com.baomidou.kisso.annotation.Login;
 import com.baomidou.kisso.annotation.Permission;
-import com.baomidou.springwind.service.IPermissionService;
 
 /**
  * <p>
@@ -21,17 +19,12 @@ import com.baomidou.springwind.service.IPermissionService;
 @Controller
 public class IndexController extends BaseController {
 
-	@Autowired
-	private IPermissionService permissionService;
-
 	/**
 	 * 首页
 	 */
 	@Permission(action = Action.Skip)
 	@RequestMapping("/index")
 	public String index(Model model) {
-		model.addAttribute("currentUser", userService.selectById(getCurrentUserId()));
-		model.addAttribute("menuList", permissionService.selectMenuVOByUserId(getCurrentUserId()));
 		return "/index";
 	}
 
