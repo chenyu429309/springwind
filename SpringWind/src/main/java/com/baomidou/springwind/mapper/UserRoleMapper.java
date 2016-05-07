ccff4b3e80d843850398fb6b769990ede5834cfe
青苗 <=>
2016-05-07 10:50:17 +0800

    演示 curd 操作

--- SpringWind/src/main/java/com/baomidou/springwind/mapper/UserRoleMapper.java
+++ SpringWind/src/main/java/com/baomidou/springwind/mapper/UserRoleMapper.java
@@ -10,5 +10,4 @@ import com.baomidou.mybatisplus.mapper.AutoMapper;
  */
 public interface UserRoleMapper extends AutoMapper<UserRole> {
 
-    void deleteByUserId(Long userId);
 }
--- SpringWind/src/main/java/com/baomidou/springwind/service/impl/RoleServiceImpl.java
+++ SpringWind/src/main/java/com/baomidou/springwind/service/impl/RoleServiceImpl.java
@@ -1,6 +1,7 @@
 package com.baomidou.springwind.service.impl;
 
 import com.baomidou.springwind.entity.Role;
+import com.baomidou.springwind.entity.UserRole;
 import com.baomidou.springwind.mapper.RoleMapper;
 import com.baomidou.springwind.mapper.UserRoleMapper;
 import com.baomidou.springwind.service.IRoleService;
@@ -16,11 +17,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class RoleServiceImpl extends BaseServiceImpl<RoleMapper, Role> implements IRoleService {
 
-    @Autowired
-    private UserRoleMapper userRoleMapper;
+	@Autowired
+	private UserRoleMapper userRoleMapper;
 
-    @Override
-    public void deleteByUserId(Long userId) {
-        userRoleMapper.deleteByUserId(userId);
-    }
+	@Override
+	public void deleteByUserId(Long userId) {
+		UserRole ur = new UserRole();
+		ur.setUid(userId);
+		userRoleMapper.deleteSelective(ur);
+	}
 }
