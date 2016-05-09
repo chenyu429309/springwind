e2896c8fd951cac69507894954504418a79c7f65
青苗 <=>
2016-05-09 22:34:36 +0800

    升级 1.1.2

--- SpringWind/src/test/resources/springwind.sql
+++ SpringWind/src/test/resources/springwind.sql
@@ -1,22 +1,22 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat MySQL Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50615
-Source Host           : localhost:3306
-Source Database       : springwind
+ Source Server         : local
+ Source Server Version : 50616
+ Source Host           : localhost
+ Source Database       : springwind
 
-Target Server Type    : MYSQL
-Target Server Version : 50615
-File Encoding         : 65001
+ Target Server Version : 50616
+ File Encoding         : utf-8
 
-Date: 2016-05-09 18:05:24
+ Date: 05/09/2016 22:34:04 PM
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for `permission`
+--  Table structure for `permission`
 -- ----------------------------
 DROP TABLE IF EXISTS `permission`;
 CREATE TABLE `permission` (
@@ -34,23 +34,14 @@ CREATE TABLE `permission` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='权限表';
 
 -- ----------------------------
--- Records of permission
+--  Records of `permission`
 -- ----------------------------
-INSERT INTO `permission` VALUES ('2', '0', '权限管理', '0', '0', '2', '/perm/', '2000', 'fa fa-eye', '权限管理');
-INSERT INTO `permission` VALUES ('1', '0', '功能管理', '0', '0', '1', '/sys/', '1000', 'fa fa-magic', '功能管理');
-INSERT INTO `permission` VALUES ('3', '0', '系统监控', '0', '0', '3', '/monitor/', '3000', 'fa fa-bar-chart-o', '系统监控');
-INSERT INTO `permission` VALUES ('4', '0', '操作日志', '0', '0', '4', '/log/', '4000', 'fa fa-bug', '操作日志');
-INSERT INTO `permission` VALUES ('11', '1', '发送邮件测试', '0', '0', '1', '/sys/mail/send.html', '1001', null, '发送邮件测试');
-INSERT INTO `permission` VALUES ('12', '1', '暂无2', '0', '0', '2', 'none2.html', '1002', null, '暂无2');
-INSERT INTO `permission` VALUES ('21', '2', '用户管理', '0', '0', '1', '/perm/user/list.html', '2001', null, '用户管理');
-INSERT INTO `permission` VALUES ('22', '2', '角色管理', '0', '0', '2', '/perm/role/list.html', '2002', null, '角色管理');
-INSERT INTO `permission` VALUES ('23', '2', '权限管理', '0', '0', '3', '/perm/permission/list.html', '2003', null, '权限管理');
-INSERT INTO `permission` VALUES ('31', '3', '警告列表', '0', '0', '1', 'abc.html', '3001', null, '警告列表');
-INSERT INTO `permission` VALUES ('32', '3', '实时监控', '0', '0', '2', '/monitor/realTime.html', '3002', null, '实时监控');
-INSERT INTO `permission` VALUES ('41', '4', '日志列表', '0', '0', '1', 'sad.htm', '4001', null, '日志列表');
+BEGIN;
+INSERT INTO `permission` VALUES ('2', '0', '权限管理', '0', '0', '2', '/perm/', '2000', 'fa fa-eye', '权限管理'), ('1', '0', '功能管理', '0', '0', '1', '/sys/', '1000', 'fa fa-magic', '功能管理'), ('3', '0', '系统监控', '0', '0', '3', '/monitor/', '3000', 'fa fa-bar-chart-o', '系统监控'), ('4', '0', '操作日志', '0', '0', '4', '/log/', '4000', 'fa fa-bug', '操作日志'), ('11', '1', '发送邮件测试', '0', '0', '1', '/sys/mail/send.html', '1001', null, '发送邮件测试'), ('12', '1', '暂无2', '0', '0', '2', 'none2.html', '1002', null, '暂无2'), ('21', '2', '用户管理', '0', '0', '1', '/perm/user/list.html', '2001', null, '用户管理'), ('22', '2', '角色管理', '0', '0', '2', '/perm/role/list.html', '2002', null, '角色管理'), ('23', '2', '权限管理', '0', '0', '3', '/perm/permission/list.html', '2003', null, '权限管理'), ('31', '3', '警告列表', '0', '0', '1', 'abc.html', '3001', null, '警告列表'), ('32', '3', '实时监控', '0', '0', '2', '/monitor/realTime.html', '3002', null, '实时监控'), ('41', '4', '日志列表', '0', '0', '1', '/log/list.html', '4001', null, '日志列表');
+COMMIT;
 
 -- ----------------------------
--- Table structure for `role`
+--  Table structure for `role`
 -- ----------------------------
 DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role` (
@@ -62,13 +53,14 @@ CREATE TABLE `role` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色表';
 
 -- ----------------------------
--- Records of role
+--  Records of `role`
 -- ----------------------------
-INSERT INTO `role` VALUES ('1', '管理员', '1', '系统管理员');
-INSERT INTO `role` VALUES ('2', '普通会员', '2', '普通会员');
+BEGIN;
+INSERT INTO `role` VALUES ('1', '管理员', '1', '系统管理员'), ('2', '普通会员', '2', '普通会员');
+COMMIT;
 
 -- ----------------------------
--- Table structure for `role_permission`
+--  Table structure for `role_permission`
 -- ----------------------------
 DROP TABLE IF EXISTS `role_permission`;
 CREATE TABLE `role_permission` (
@@ -79,28 +71,14 @@ CREATE TABLE `role_permission` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='角色权限表';
 
 -- ----------------------------
--- Records of role_permission
+--  Records of `role_permission`
 -- ----------------------------
-INSERT INTO `role_permission` VALUES ('1', '1', '1');
-INSERT INTO `role_permission` VALUES ('2', '1', '2');
-INSERT INTO `role_permission` VALUES ('3', '1', '3');
-INSERT INTO `role_permission` VALUES ('4', '1', '4');
-INSERT INTO `role_permission` VALUES ('5', '1', '11');
-INSERT INTO `role_permission` VALUES ('6', '1', '12');
-INSERT INTO `role_permission` VALUES ('7', '1', '21');
-INSERT INTO `role_permission` VALUES ('8', '1', '22');
-INSERT INTO `role_permission` VALUES ('9', '1', '23');
-INSERT INTO `role_permission` VALUES ('10', '1', '31');
-INSERT INTO `role_permission` VALUES ('11', '1', '32');
-INSERT INTO `role_permission` VALUES ('12', '1', '41');
-INSERT INTO `role_permission` VALUES ('13', '2', '2');
-INSERT INTO `role_permission` VALUES ('14', '2', '21');
-INSERT INTO `role_permission` VALUES ('15', '2', '3');
-INSERT INTO `role_permission` VALUES ('16', '2', '31');
-INSERT INTO `role_permission` VALUES ('17', '2', '32');
+BEGIN;
+INSERT INTO `role_permission` VALUES ('1', '1', '1'), ('2', '1', '2'), ('3', '1', '3'), ('4', '1', '4'), ('5', '1', '11'), ('6', '1', '12'), ('7', '1', '21'), ('8', '1', '22'), ('9', '1', '23'), ('10', '1', '31'), ('11', '1', '32'), ('12', '1', '41'), ('13', '2', '2'), ('14', '2', '21'), ('15', '2', '3'), ('16', '2', '31'), ('17', '2', '32');
+COMMIT;
 
 -- ----------------------------
--- Table structure for `sys_log`
+--  Table structure for `sys_log`
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_log`;
 CREATE TABLE `sys_log` (
@@ -108,12 +86,19 @@ CREATE TABLE `sys_log` (
   `uid` bigint(20) unsigned DEFAULT NULL COMMENT '用户ID',
   `content` varchar(600) NOT NULL DEFAULT '' COMMENT '日志内容',
   `operation` varchar(250) DEFAULT NULL COMMENT '用户操作',
-  `crtime` datetime NOT NULL COMMENT '创建时间',
+  `crTime` datetime NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=423769215225298945 DEFAULT CHARSET=utf8 COMMENT='操作日志表';
+) ENGINE=InnoDB AUTO_INCREMENT=423829597029662723 DEFAULT CHARSET=utf8 COMMENT='操作日志表';
 
 -- ----------------------------
--- Table structure for `user`
+--  Records of `sys_log`
+-- ----------------------------
+BEGIN;
+INSERT INTO `sys_log` VALUES ('423827573609332736', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:password=abc&loginName=abc&,[IP]:10.10.10.10', '登录', '2016-05-09 21:55:10'), ('423827889272651776', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:password=abc&loginName=admin&,[IP]:10.10.10.10', '登录', '2016-05-09 21:56:23'), ('423828945075437568', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:password=acbbb&repassword=acbbb&loginName=acbbb&,[IP]:10.10.10.10', '登录', '2016-05-09 22:00:37'), ('423828960116211712', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:insertSelective,[参数]:password=acbbb&repassword=acbbb&loginName=acbbb&,[IP]:10.10.10.10', '添加用户', '2016-05-09 22:00:40'), ('423829494923526144', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:password=abc&loginName=admin&,[IP]:10.10.10.10', '登录', '2016-05-09 22:02:48'), ('423829536409387008', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:password=abc&loginName=admin&,[IP]:10.10.10.10', '登录', '2016-05-09 22:02:58'), ('423829555678019584', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:password=123&loginName=admin&,[IP]:10.10.10.10', '登录', '2016-05-09 22:03:02'), ('423829597029662720', '1', '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:deleteUser,[参数]:null,[IP]:10.10.10.10', '删除用户', '2016-05-09 22:03:12'), ('423829597029662721', '1', '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:deleteUser,[参数]:null,[IP]:10.10.10.10', '删除用户', '2016-05-09 22:03:12');
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `user`
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
@@ -129,17 +114,14 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户表';
 
 -- ----------------------------
--- Records of user
+--  Records of `user`
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', 'admin', 'b9140469ac5d2a70d86a583e9095ad6f', null, '0', '1', '2016-04-17 18:52:26', '2016-04-17 18:52:26');
-INSERT INTO `user` VALUES ('2', 'test', 'd227f9edb541169068b11221869a014a', null, '0', '1', '2016-04-14 18:22:58', '2016-04-14 18:22:58');
-INSERT INTO `user` VALUES ('415112286823251968', 'abc', 'c4c99e1a5bd140d138ad0863bf59acb3', null, '0', '1', '2016-04-15 20:43:43', '2016-04-15 20:43:43');
-INSERT INTO `user` VALUES ('415112325381488640', 'def', '94e5398201cd9452b1e667044debcd10', null, '0', '1', '2016-04-15 20:43:53', '2016-04-15 20:43:53');
-INSERT INTO `user` VALUES ('423730216519598080', 'asddsa', '539228ca62df02143c88137ca0dc5c13', null, '0', '1', '2016-05-09 15:28:18', '2016-05-09 15:28:18');
-INSERT INTO `user` VALUES ('423734169042944000', 'sdddd', '9245c4eafd3aa6884fca10d8b60c057c', null, '0', '1', '2016-05-09 15:44:00', '2016-05-09 15:44:00');
+BEGIN;
+INSERT INTO `user` VALUES ('1', 'admin', 'b9140469ac5d2a70d86a583e9095ad6f', null, '0', '1', '2016-04-17 18:52:26', '2016-04-17 18:52:26'), ('2', 'test', 'd227f9edb541169068b11221869a014a', null, '0', '1', '2016-04-14 18:22:58', '2016-04-14 18:22:58'), ('415112286823251968', 'abc', 'c4c99e1a5bd140d138ad0863bf59acb3', null, '0', '1', '2016-04-15 20:43:43', '2016-04-15 20:43:43'), ('415112325381488640', 'def', '94e5398201cd9452b1e667044debcd10', null, '0', '1', '2016-04-15 20:43:53', '2016-04-15 20:43:53');
+COMMIT;
 
 -- ----------------------------
--- Table structure for `user_role`
+--  Table structure for `user_role`
 -- ----------------------------
 DROP TABLE IF EXISTS `user_role`;
 CREATE TABLE `user_role` (
@@ -150,7 +132,10 @@ CREATE TABLE `user_role` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户角色表';
 
 -- ----------------------------
--- Records of user_role
+--  Records of `user_role`
 -- ----------------------------
-INSERT INTO `user_role` VALUES ('1', '1', '1');
-INSERT INTO `user_role` VALUES ('2', '2', '2');
+BEGIN;
+INSERT INTO `user_role` VALUES ('1', '1', '1'), ('2', '2', '2');
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
