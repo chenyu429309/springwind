8c91e40e424e378620b73940388ac5a4194a3de6
青苗 <=>
2016-07-24 20:20:38 +0800

    更新数据结构

--- SpringWind/src/test/resources/springwind.sql
+++ SpringWind/src/test/resources/springwind.sql
@@ -9,36 +9,11 @@
  Target Server Version : 50616
  File Encoding         : utf-8
 
- Date: 05/11/2016 22:38:17 PM
+ Date: 07/24/2016 20:20:02 PM
 */
 
 SET NAMES utf8;
 SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
---  Table structure for `permission`
--- ----------------------------
-DROP TABLE IF EXISTS `permission`;
-CREATE TABLE `permission` (
-  `id` bigint(20) NOT NULL COMMENT '主键',
-  `pid` bigint(20) NOT NULL DEFAULT '0' COMMENT '上级ID',
-  `title` varchar(30) COLLATE utf8_bin NOT NULL COMMENT '标题',
-  `type` smallint(6) NOT NULL DEFAULT '0' COMMENT '类型 0、菜单 1、功能',
-  `state` smallint(6) NOT NULL DEFAULT '0' COMMENT '状态 0、正常 1、禁用',
-  `sort` int(11) NOT NULL DEFAULT '0' COMMENT '排序',
-  `url` varchar(100) COLLATE utf8_bin DEFAULT NULL COMMENT '地址',
-  `permCode` varchar(30) COLLATE utf8_bin DEFAULT NULL COMMENT '权限编码',
-  `icon` varchar(30) COLLATE utf8_bin DEFAULT NULL COMMENT '图标',
-  `description` varchar(80) COLLATE utf8_bin NOT NULL COMMENT '描述',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='权限表';
-
--- ----------------------------
---  Records of `permission`
--- ----------------------------
-BEGIN;
-INSERT INTO `permission` VALUES ('2', '0', '权限管理', '0', '0', '2', '/perm/', '2000', 'fa fa-eye', '权限管理'), ('1', '0', '功能管理', '0', '0', '1', '/sys/', '1000', 'fa fa-magic', '功能管理'), ('3', '0', '系统监控', '0', '0', '3', '/monitor/', '3000', 'fa fa-bar-chart-o', '系统监控'), ('4', '0', '操作日志', '0', '0', '4', '/log/', '4000', 'fa fa-bug', '操作日志'), ('11', '1', '发送邮件测试', '0', '0', '1', '/sys/mail/send.html', '1001', null, '发送邮件测试'), ('12', '1', 'Echarts 测试', '0', '0', '2', '/sys/echarts/map.html', '1002', null, '暂无2'), ('21', '2', '用户管理', '0', '0', '1', '/perm/user/list.html', '2001', null, '用户管理'), ('22', '2', '角色管理', '0', '0', '2', '/perm/role/list.html', '2002', null, '角色管理'), ('23', '2', '权限管理', '0', '0', '3', '/perm/permission/list.html', '2003', null, '权限管理'), ('31', '3', '警告列表', '0', '0', '1', 'abc.html', '3001', null, '警告列表'), ('32', '3', '实时监控', '0', '0', '2', '/monitor/realTime.html', '3002', null, '实时监控'), ('41', '4', '日志列表', '0', '0', '1', '/log/list.html', '4001', null, '日志列表');
-COMMIT;
 
 -- ----------------------------
 --  Table structure for `role`
@@ -56,7 +31,7 @@ CREATE TABLE `role` (
 --  Records of `role`
 -- ----------------------------
 BEGIN;
-INSERT INTO `role` VALUES ('1', '管理员', '1', '系统管理员'), ('2', '普通会员', '2', '普通会员');
+INSERT INTO `role` VALUES ('1', '管理员', '1', '系统管理员'), ('2', '普通会员', '2', '普通会员'), ('424917074352013312', '测试1', '1', '测试1');
 COMMIT;
 
 -- ----------------------------
@@ -74,7 +49,7 @@ CREATE TABLE `role_permission` (
 --  Records of `role_permission`
 -- ----------------------------
 BEGIN;
-INSERT INTO `role_permission` VALUES ('1', '1', '1'), ('2', '1', '2'), ('3', '1', '3'), ('4', '1', '4'), ('5', '1', '11'), ('6', '1', '12'), ('7', '1', '21'), ('8', '1', '22'), ('9', '1', '23'), ('10', '1', '31'), ('11', '1', '32'), ('12', '1', '41'), ('13', '2', '2'), ('14', '2', '21'), ('15', '2', '3'), ('16', '2', '31'), ('17', '2', '32');
+INSERT INTO `role_permission` VALUES ('100', '1', '100'), ('110', '1', '110'), ('111', '1', '111'), ('112', '1', '112'), ('113', '1', '113'), ('120', '1', '120'), ('121', '1', '121'), ('122', '1', '122'), ('123', '1', '123'), ('200', '1', '200'), ('210', '1', '210'), ('211', '1', '211'), ('212', '1', '212'), ('213', '1', '213'), ('220', '1', '220'), ('221', '1', '221'), ('300', '1', '300'), ('310', '1', '310'), ('311', '1', '311'), ('312', '1', '312'), ('313', '1', '313');
 COMMIT;
 
 -- ----------------------------
@@ -88,13 +63,38 @@ CREATE TABLE `sys_log` (
   `operation` varchar(250) DEFAULT NULL COMMENT '用户操作',
   `crTime` datetime NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=424538725716852737 DEFAULT CHARSET=utf8 COMMENT='操作日志表';
+) ENGINE=InnoDB AUTO_INCREMENT=451013944245485569 DEFAULT CHARSET=utf8 COMMENT='操作日志表';
 
 -- ----------------------------
 --  Records of `sys_log`
 -- ----------------------------
 BEGIN;
-INSERT INTO `sys_log` VALUES ('423827573609332736', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:password=abc&loginName=abc&,[IP]:10.10.10.10', '登录', '2016-05-09 21:55:10'), ('423827889272651776', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:password=abc&loginName=admin&,[IP]:10.10.10.10', '登录', '2016-05-09 21:56:23'), ('423828945075437568', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:password=acbbb&repassword=acbbb&loginName=acbbb&,[IP]:10.10.10.10', '登录', '2016-05-09 22:00:37'), ('423828960116211712', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:insertSelective,[参数]:password=acbbb&repassword=acbbb&loginName=acbbb&,[IP]:10.10.10.10', '添加用户', '2016-05-09 22:00:40'), ('423829494923526144', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:password=abc&loginName=admin&,[IP]:10.10.10.10', '登录', '2016-05-09 22:02:48'), ('423829536409387008', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:password=abc&loginName=admin&,[IP]:10.10.10.10', '登录', '2016-05-09 22:02:58'), ('423829555678019584', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:password=123&loginName=admin&,[IP]:10.10.10.10', '登录', '2016-05-09 22:03:02'), ('423829597029662720', '1', '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:deleteUser,[参数]:null,[IP]:10.10.10.10', '删除用户', '2016-05-09 22:03:12'), ('423829597029662721', '1', '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:deleteUser,[参数]:null,[IP]:10.10.10.10', '删除用户', '2016-05-09 22:03:12'), ('424204895713755136', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:password=123&,[IP]:10.10.10.10', '登录', '2016-05-10 22:54:30'), ('424204946615828480', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:password=abc&loginName=admin&,[IP]:10.10.10.10', '登录', '2016-05-10 22:54:42'), ('424204971169284096', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:password=123&loginName=admin&,[IP]:10.10.10.10', '登录', '2016-05-10 22:54:48'), ('424212917953495040', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:password=123&,[IP]:10.10.10.10', '登录', '2016-05-10 23:26:23'), ('424213509627183104', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:password=123&,[IP]:10.10.10.10', '登录', '2016-05-10 23:28:44'), ('424213551029157888', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:password=123&loginName=admin&,[IP]:10.10.10.10', '登录', '2016-05-10 23:28:54'), ('424538597211766784', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:password=abc&loginName=abc&,[IP]:10.10.10.10', '登录', '2016-05-11 21:00:31'), ('424538650328432640', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:password=abc&loginName=admin&,[IP]:10.10.10.10', '登录', '2016-05-11 21:00:44'), ('424538669534150656', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:password=abc&loginName=admin&,[IP]:10.10.10.10', '登录', '2016-05-11 21:00:48'), ('424538690912518144', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:password=123&loginName=admin&,[IP]:10.10.10.10', '登录', '2016-05-11 21:00:53'), ('424538709661057024', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:password=123&,[IP]:10.10.10.10', '登录', '2016-05-11 21:00:58'), ('424538725716852736', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:password=123&,[IP]:10.10.10.10', '登录', '2016-05-11 21:01:02');
+INSERT INTO `sys_log` VALUES ('447024627907035136', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:ctoken=8228c32e311e4be8841afe617270c465&password=123&captcha=nwpa&loginName=admin&,[IP]:192.168.11.102', '登录', '2016-07-12 22:11:58'), ('447024707498147840', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:ctoken=b5f899bd139b40d09848d9be87c3674c&password=123&captcha=gnd4&loginName=test&,[IP]:192.168.11.102', '登录', '2016-07-12 22:12:17'), ('448853784936841216', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:ctoken=b3b84b04f2154cfd921bc202945f5e69&password=123&captcha=bss2&loginName=admin&,[IP]:10.10.10.4', '登录', '2016-07-17 23:20:23'), ('449228946764988416', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:ctoken=418f346329f44c588e518ba6fc22b0d3&password=123&captcha=9knn&loginName=admin&,[IP]:192.168.11.102', '登录', '2016-07-19 00:11:09'), ('449229230278967296', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:ctoken=00dc18ed5da94735a8880389ece44d7e&password=123&captcha=9db5&loginName=admin&,[IP]:192.168.11.102', '登录', '2016-07-19 00:12:17'), ('450926100940324864', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:ctoken=de1cd3d4e010438d81bbb13aabb90450&password=123&captcha=5keg&loginName=admin&,[IP]:192.168.11.102', '登录', '2016-07-23 16:35:02'), ('451013944245485568', null, '[类名]:com.baomidou.springwind.service.impl.UserServiceImpl,[方法]:selectByLoginName,[参数]:ctoken=9a7228b599e04b4c9f10b3424e198753&password=123&captcha=km93&loginName=admin&,[IP]:10.10.10.3', '登录', '2016-07-23 22:24:06');
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `sys_permission`
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_permission`;
+CREATE TABLE `sys_permission` (
+  `id` bigint(20) NOT NULL COMMENT '主键',
+  `pid` bigint(20) NOT NULL DEFAULT '0' COMMENT '上级ID',
+  `title` varchar(30) COLLATE utf8_bin NOT NULL COMMENT '标题',
+  `type` smallint(6) NOT NULL DEFAULT '0' COMMENT '类型 0、菜单 1、功能',
+  `state` smallint(6) NOT NULL DEFAULT '0' COMMENT '状态 0、正常 1、禁用',
+  `sort` int(11) NOT NULL DEFAULT '0' COMMENT '排序',
+  `url` varchar(100) COLLATE utf8_bin DEFAULT NULL COMMENT '地址',
+  `permCode` varchar(30) COLLATE utf8_bin DEFAULT NULL COMMENT '权限编码',
+  `icon` varchar(30) COLLATE utf8_bin DEFAULT NULL COMMENT '图标',
+  `description` varchar(80) COLLATE utf8_bin NOT NULL COMMENT '描述',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='权限表';
+
+-- ----------------------------
+--  Records of `sys_permission`
+-- ----------------------------
+BEGIN;
+INSERT INTO `sys_permission` VALUES ('200', '0', '系统管理', '0', '0', '1', '#', '2000', 'fa fa-eye', '系统管理'), ('210', '200', '功能权限', '0', '0', '0', '#', '2000', 'fa fa-magic', '功能权限'), ('220', '200', '系统日志', '0', '0', '1', '#', '2000', '系统日志', '系统监控'), ('221', '220', '操作日志', '0', '0', '4', '/log/list.html', '2000', 'fa fa-bug', '操作日志'), ('113', '110', '发送邮件测试', '0', '0', '1', '/mail/send.html', '1001', null, '发送邮件测试'), ('313', '310', '雷达图', '0', '0', '0', null, '3000', null, '雷达图'), ('211', '210', '用户管理', '0', '0', '0', '/user/list.html', '2001', null, '用户管理'), ('212', '210', '角色管理', '0', '0', '1', '/role/list.html', '2002', null, '角色管理'), ('213', '210', '菜单管理', '0', '0', '2', '/permission/menu.html', '2003', null, '菜单管理'), ('310', '300', 'echarts 图表', '0', '0', '0', '#', '3000', null, 'echarts 图表'), ('311', '310', '折线图', '0', '0', '0', null, '3000', null, '折线图'), ('312', '310', '散列图', '0', '0', '0', null, '3000', null, '散列图'), ('110', '100', '支持', '0', '0', '0', '#', '1000', null, '支持'), ('300', '0', '统计分析', '0', '0', '2', '#', '3000', null, '统计分析'), ('100', '0', '功能管理', '0', '0', '0', '#', '1000', null, '功能管理'), ('111', '110', '捐赠 - 作者', '0', '0', '0', '/donate.html', '1000', null, '捐赠作者'), ('112', '110', '捐赠 - 列表', '0', '0', '1', '/donate/list.html', '1000', null, '捐赠 - 列表'), ('120', '100', '异常', '0', '0', '1', '#', '1000', null, '异常'), ('121', '120', '403', '0', '0', '0', '/error/403.html', '1000', null, '403 异常'), ('122', '120', '404', '0', '0', '1', '/error/404.html', '1000', null, '404 异常'), ('123', '120', '500', '0', '0', '2', '/error/500.html', '1000', null, '500 异常');
 COMMIT;
 
 -- ----------------------------
@@ -117,7 +117,7 @@ CREATE TABLE `user` (
 --  Records of `user`
 -- ----------------------------
 BEGIN;
-INSERT INTO `user` VALUES ('1', 'admin', 'b9140469ac5d2a70d86a583e9095ad6f', null, '0', '1', '2016-04-17 18:52:26', '2016-04-17 18:52:26'), ('2', 'test', 'd227f9edb541169068b11221869a014a', null, '0', '1', '2016-04-14 18:22:58', '2016-04-14 18:22:58'), ('415112286823251968', 'abc', 'c4c99e1a5bd140d138ad0863bf59acb3', null, '0', '1', '2016-04-15 20:43:43', '2016-04-15 20:43:43'), ('415112325381488640', 'def', '94e5398201cd9452b1e667044debcd10', null, '0', '1', '2016-04-15 20:43:53', '2016-04-15 20:43:53');
+INSERT INTO `user` VALUES ('1', 'admin', 'b9140469ac5d2a70d86a583e9095ad6f', 'abc@a.com', '1', '1', '2016-04-17 18:52:26', '2016-04-17 18:52:26'), ('2', 'test', 'd227f9edb541169068b11221869a014a', 'abc1@a.com', '1', '1', '2016-04-14 18:22:58', '2016-04-14 18:22:58'), ('415112286823251968', 'abc', 'c4c99e1a5bd140d138ad0863bf59acb3', 'abc2@a.com', '1', '0', '2016-04-15 20:43:43', '2016-04-15 20:43:43'), ('415112325381488640', 'def', '94e5398201cd9452b1e667044debcd10', 'abc3@a.com', '0', '1', '2016-04-15 20:43:53', '2016-04-15 20:43:53');
 COMMIT;
 
 -- ----------------------------
@@ -135,7 +135,7 @@ CREATE TABLE `user_role` (
 --  Records of `user_role`
 -- ----------------------------
 BEGIN;
-INSERT INTO `user_role` VALUES ('1', '1', '1'), ('2', '2', '2');
+INSERT INTO `user_role` VALUES ('1', '1', '1');
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
